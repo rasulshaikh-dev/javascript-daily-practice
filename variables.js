4f1dec30e1a33c84f0ec4dabe9c13a539f43a699
Rasul Shaikh <rasulshaikh.dev@gmail.com>
2026-03-06 05:12:14 +0530

    JavaScript Variables Practice
    Added variables.js explaining const, let, and var

---
+++ variables.js
@@ -0,0 +1,16 @@
+
+// const for values that should never change
+
+const accountId = 1234567890;
+const accountPassword = "7896525210";
+
+// let for variables that may change
+
+let currentScore = 10;
+currentScore = 15;  // The value is now updated to 15
+
+// var (old way - avoid in modern JavaScript)
+
+var oldMessage = "Hello, World";
+
+console.table([accountId, accountPassword, currentScore, oldMessage,]);

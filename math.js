ec69f5fb90830d6726e28b07809292dc698b1dd6
Rasul Shaikh <rasulshaikh.dev@gmail.com>
2026-03-08 21:03:58 +0530

    Javascript Math Methods
    Math.round(),Math.floor(), Math.ceil(), Math.trunc(), Math.trunc(),Math.max(), Math.min(), Math.pow(),Math.sqrt(), Math.abs(), Math.cbrt(), Math.log(),Math.log10(), Math.sin(), Math.cos(), Math.tan().

---
+++ math.js
@@ -0,0 +1,101 @@
+
+// Math Methods
+
+// 1) Math.round()
+// Rounds a number to the nearest integer
+
+let num1 = 4.6;
+console.log(Math.round(num1)); 
+
+
+// 2) Math.floor()
+// Rounds a number DOWN to the nearest integer
+
+let num2 = 4.9;
+console.log(Math.floor(num2)); 
+
+
+// 3) Math.ceil()
+// Rounds a number UP to the nearest integer
+
+let num3 = 4.1;
+console.log(Math.ceil(num3)); 
+
+
+// 4) Math.trunc()
+// Removes the decimal part of a number
+
+let num4 = 4.9;
+console.log(Math.trunc(num4)); 
+
+
+// 5) Math.random()
+// Generates a random number between 0 and 1
+
+console.log(Math.random());
+
+
+// 6) Math.max()
+// Returns the largest number
+
+console.log(Math.max(10, 20, 50, 5)); 
+
+
+// 7) Math.min()
+// Returns the smallest number
+
+console.log(Math.min(10, 20, 50, 5)); 
+
+
+// 8) Math.pow()
+// Returns the value of a number raised to a power
+
+console.log(Math.pow(2, 3)); 
+
+
+// 9) Math.sqrt()
+// Returns the square root of a number
+
+console.log(Math.sqrt(25)); 
+
+
+// 10) Math.abs()
+// Returns the absolute (positive) value of a number
+
+console.log(Math.abs(-15)); 
+
+
+// 11) Math.cbrt()
+// Returns the cube root of a number
+
+console.log(Math.cbrt(27)); 
+
+
+// 12) Math.log()
+// Returns the natural logarithm of a number
+
+console.log(Math.log(1)); 
+
+
+// 13) Math.log10()
+// Returns the base-10 logarithm of a number
+
+console.log(Math.log10(100)); 
+
+
+// 14) Math.sin()
+// Returns the sine of an angle (in radians)
+
+console.log(Math.sin(0)); 
+
+
+// 15) Math.cos()
+// Returns the cosine of an angle (in radians)
+
+console.log(Math.cos(0)); 
+
+
+// 16) Math.tan()
+// Returns the tangent of an angle (in radians)
+
+console.log(Math.tan(0)); 

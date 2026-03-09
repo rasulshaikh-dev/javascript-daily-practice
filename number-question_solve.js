dbc089b0d6ae55af42e3e248a1287ab9623b465f
Rasul Shaikh <rasulshaikh.dev@gmail.com>
2026-03-09 12:33:36 +0530

    javascript-numbers-practice.js
    JavaScript practice file covering Numbers  methods with beginner-friendly examples and explanations.

---
+++ number-question_solve.js
@@ -0,0 +1,86 @@
+
+
+// ======================================
+// JavaScript Numbers - Questions & Answers
+// ======================================
+
+
+// 1) Create a variable num with value 25 and print its type.
+
+let num = 25;
+console.log(typeof num);
+
+
+// 2) Convert the string "100" into a number using Number().
+
+let num1 = Number("100");
+console.log(num1);
+
+
+// 3) Use parseInt() to extract a number from "50px".
+
+console.log(parseInt("50px"));
+
+
+// 4) Convert the number 300 into a string using toString().
+
+let num3 = 300;
+console.log(num3.toString());
+
+
+// 5) Check if 15 is an integer using Number.isInteger().
+
+console.log(Number.isInteger(15));
+
+
+// 6) Convert "45.89" into a number using parseFloat().
+
+console.log(parseFloat("45.89"));
+
+
+// 7) Format 9.4567 to 2 decimal places using toFixed().
+
+let num5 = 9.4567;
+console.log(num5.toFixed(2));
+
+
+// 8) Format 12.3456 to 3 significant digits using toPrecision().
+
+let num6 = 12.3456;
+console.log(num6.toPrecision(3));
+
+
+// 9) Check if 25.5 is an integer using Number.isInteger().
+
+console.log(Number.isInteger(25.5));
+
+
+// 10) Convert "150" into a number and print its type.
+
+let num7 = Number("150");
+console.log(num7, typeof num7);
+
+
+// 11) Convert the number 255 into binary.
+
+let num8 = 255;
+console.log(num8.toString(2));
+
+
+// 12) Convert the number 100 into hexadecimal.
+
+let num9 = 100;
+console.log(num9.toString(16));
+
+
+// 13) Write code that produces NaN.
+
+console.log("hello" / 2);
+
+
+// 14) Write code that produces Infinity.
+
+console.log(10 / 0);
+
+
+
